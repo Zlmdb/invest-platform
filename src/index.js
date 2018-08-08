e312711dc55a264a715c0070e511957c70998fd7
Tony <htcm8w@163.com>
2018-08-08 17:55:59 +0800

    xx

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
 import Router from './router';
 import registerServiceWorker from './registerServiceWorker';
-
 import { Provider } from 'react-redux';
 import store from './store'
 
@@ -15,23 +13,21 @@ window.onresize = () => {
     let deviceWidth = document.documentElement.clientWidth;
     document.documentElement.style.fontSize = deviceWidth / 19.2 + 'px';
 }
-// class App extends React.Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {}
-//     }
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>
+                {Router()}
+            </div>
+        )
+    }
+}
 
-//     render() {
-//         return (
-//             <div>
-//                 {Router()}
-//             </div>
-//         )
-//     }
-// }
 
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Provider store={store} ><Router /></Provider>, document.getElementById('root'));
-// ReactDOM.render(Router(), document.getElementById('root'));
+// ReactDOM.render(Router(),document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App/></Provider>,document.getElementById('root'));
 registerServiceWorker();
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,10 @@
 import React from 'react'
-import { Route,  BrowserRouter } from 'react-router-dom'
-
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import App from 'view/app'
 import About from 'view/about'
 import Question from 'view/question'
 
-// export default function () {
-//     return (
-//         <div>
-//             <BrowserRouter>
-//                 <div>
-//                     <Route exact path='/' component={App} />
-//                     <Route path='/about' component={About} />
-//                     <Route path='/question' component={Question} />
-//                 </div>
-//             </BrowserRouter>
-//         </div>
-//     )
-// }
-const rouder=() => {
+export default function () {
     return (
         <div>
             <BrowserRouter>
@@ -32,4 +18,3 @@ const rouder=() => {
     )
 }
 
-export default rouder
